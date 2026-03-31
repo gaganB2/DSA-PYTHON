1aa2a27c933576170228d2ef2ce2c01a77b9e74b
Gagan Sahu <gagansahu55428@gmail.com>
2026-03-31 10:51:32 +0530

    Array Traversals 	new file:   Array/Traversals.py

---
+++ Array/Traversals.py
@@ -0,0 +1,20 @@
+arr = [3,7,2,9,4]
+# Maximum Value
+max_value = arr[0]
+for i in arr:
+    if i>max_value:
+        max_value = i
+print(max_value)
+
+# Minimum Value
+minimumvalue = arr[0]
+for comparingnumbers in arr:
+    if comparingnumbers < minimumvalue:
+        minimumvalue = comparingnumbers
+print(minimumvalue)
+
+#Sum of Array
+total = 0
+for numbers in arr:
+    total += numbers
+print(total)
